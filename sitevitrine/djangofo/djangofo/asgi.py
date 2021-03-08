d3a325046442a1c5a0676ac92aa4ed385940adbb
ptp <paartheepan94@gmail.com>
2021-03-08 09:49:56 +0100

    docs: add readme

--- sitevitrine/djangofo/djangofo/asgi.py
+++ sitevitrine/djangofo/djangofo/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for sitevitrine project.
+ASGI config for djangofo project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sitevitrine.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangofo.settings')
 
 application = get_asgi_application()
