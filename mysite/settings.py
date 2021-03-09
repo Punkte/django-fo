36cbaffefed002d7d0761c9e51f91c276c2a4358
ptp <paartheepan94@gmail.com>
2021-03-09 15:26:23 +0100

    some settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,4 +139,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'formation/static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'formation/static')]
