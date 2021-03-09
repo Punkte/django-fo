223dbdc17e8c1d57905b84679cb12e99df741f19
adrienbannwarth <adrien.bann@gmail.com>
2021-03-09 09:13:57 +0100

    push j1

--- mysite/formation/urls.py
+++ mysite/formation/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('formation/', include([
+        path('', views.index, name='index'),
+        path('blog/', views.blog, name='blog'),
+        path('post/<int:pk>/', views.post_detail, name='post_detail'),
+        path('post/new/', views.post_new, name='post_new'),
+    ])),
 ]
--- mysite/formation/views.py
+++ mysite/formation/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
+from .models import Post
+from django.utils import timezone
 from django.shortcuts import render
 from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
 
 # Create your views here.
 def index(request):
@@ -10,6 +14,24 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'formation/index.html', context)
 
-def detail(request, question_id):
-  question = get_object_or_404(Question, pk=question_id)
-  return render(request, 'formation/detail.html', {'question': question})
+def blog(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    context = {'posts': posts}
+    return render(request, 'blog/index.html', context)
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
